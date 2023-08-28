eb68e7086d7383047c1d74350cf61eae3bd9c4f5
MrLawrenceAwe <mrlawrenceawe@gmail.com>
2023-08-28 10:48:41 +0100

    Removed test comment

--- src/main/java/com/LawrenceAwe/artifact/WordsAPIApplication.java
+++ src/main/java/com/LawrenceAwe/artifact/WordsAPIApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WordsAPIApplication {
 
 	public static void main(String[] args) {
-		//Test comment
 		SpringApplication.run(WordsAPIApplication.class, args);
 	}
 }
